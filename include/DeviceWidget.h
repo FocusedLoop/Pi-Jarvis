a7553391a06192c7e992be6287e0dfb476628c17
Joshua Wlodarczyk <joshuawlod2003@gmail.com>
2025-12-29 20:57:00 +1000

    still working on status indicator code

--- include/DeviceWidget.h
+++ include/DeviceWidget.h
@@ -1,7 +1,9 @@
 #ifndef DEVICE_WIDGET_H
 #define DEVICE_WIDGET_H
 
+#pragma once // LOOK INTO MORE
 #include <gtkmm.h>
+#include "ConnectionState.h"
 
 class DeviceWidget : public Gtk::Box
 {
@@ -9,6 +11,19 @@ public:
     DeviceWidget();
 
 private:
+	// Status Indicators
+    Gtk::DrawingArea ssh_dot_;
+	Gtk::DrawingArea esp32_dot_;
+
+    ConnectionStatus ssh_status_ = ConnectionStatus::DISCONNECTED;
+	ConnectionStatus esp32_status_ = ConnectionStatus::DISCONNECTED;
+
+	void on_state_changed(ConnectionSnapshot snapshot);
+    void setup_dot(Gtk::DrawingArea& dot);
+
+    void draw_dot(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height,, ConnectionStatus status)// Callback
+
+    // Button
     Gtk::Box m_button_box;
 };
 
--- src/DeviceWidget.cpp
+++ src/DeviceWidget.cpp
@@ -1,6 +1,7 @@
 #include "PiJ.h"
 #include "DeviceWidget.h"
 #include "Button.h"
+#include <cmath>
 
 // TODO: Add status indicators for device and SSH connection states
 
@@ -15,8 +16,18 @@ namespace {
         button.height = 100;
         return button;
     }
+
+	// Set colour based on connection status
+    void set_colour(const Cairo::RefPtr<Cairo::Context>& cr, ConnectionStatus status) {
+        switch (status) {
+            case ConnectionStatus::CONNECTED:cr->set_source_rgb(0.1, 0.8, 0.1); break; // Green
+			case ConnectionStatus::DISCONNECTED:cr->set_source_rgb(0.9, 0.2, 0.2); break; // Red
+			default: cr->set_source_rgb(0.5, 0.5, 0.5); break; // Grey
+        }
+    }
 }
 
+// TODO: IMPLEMENT DEVICE DOT
 DeviceWidget::DeviceWidget() : Gtk::Box(Gtk::Orientation::VERTICAL, 10)
 {
     set_expand(true);
@@ -31,3 +42,11 @@ DeviceWidget::DeviceWidget() : Gtk::Box(Gtk::Orientation::VERTICAL, 10)
 
     append(*button1);
 }
+
+// TODO: IMPLEMENT DEVICE DOT SETUP
+
+
+// TODO: ON STATE CHANGED
+
+
+// TODO: DRAW DOT
